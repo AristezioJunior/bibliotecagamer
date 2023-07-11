5131d941cdc947539adb1405e63dfa91843192ac
Aristezio Junior <aristezio@gmail.com>
2023-07-11 16:36:42 -0300

    Criando projeto para desenvolvimento de um backend de uma biblioteca gamer

---
+++ src/main/java/com/bibliotecagamer/BibliotecaGamerApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotecagamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaGamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibliotecagamer/BibliotecaGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotecagamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
